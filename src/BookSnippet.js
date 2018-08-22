7e412da58fe6f7cbacdad357c684b333d66971c4
Renan San <renaansan@gmail.com>
2018-08-21 23:45:20 -0300

    Turn back to an stateful component to fix shelfSelector
    select of shelfs now show the current shelf of the book properly and BookSnippet component start using shelfSelector component

--- src/BookSnippet.js
+++ src/BookSnippet.js
@@ -2,35 +2,53 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import slugify from 'slugify'
 import PropTypes from 'prop-types'
+import ShelfSelector from './ShelfSelector'
+import * as BooksAPI from './BooksAPI'
 
-const BookSnippet = (props) => {
-  const change = (event) => {
+class BookSnippet extends React.Component {
+
+  //handle changes on ShelfSelector
+  handleChange = (event) => {
     const el = event.target;
     const shelf = {
       id: el.value,
       title: el[el.selectedIndex].textContent
     }
-    props.updateShelf(props.details, shelf)
+    this.props.updateShelf(this.props.details, shelf)
   };
-  const {id, title, authors, imageLinks, shelf} = props.details;
-  const cover = (imageLinks && imageLinks.thumbnail) ? imageLinks.thumbnail : '';
+
   // const linkTo = `/book/${slugify(title, {lower: true})}`;
   // const linkTo = `/book/${slugify(title, {lower: true})}#${id}`;
   // const linkTo = `/book/${slugify(title, {lower: true})}?id=${id}`;
 
-  return (
-    <li>
+  state = {
+    currentShelf: '',
+  }
+
+  componentDidMount() {
+    // const bookId = this.props.location.hash.replace('#', '');
+    // const bookId = this.props.location.state.bookId;
+    if (this.props.details) {
+      BooksAPI.get(this.props.details.id).then(book => {
+        this.setState({ currentShelf: book.shelf });
+      });
+    }
+  }
+
+  render() {
+    const {id, title, authors, imageLinks} = this.props.details;
+    const cover = (imageLinks && imageLinks.thumbnail) ? imageLinks.thumbnail : '';
+    let shelf = this.props.details.shelf || this.state.currentShelf;
+    // debugger;
+    return (
+      <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${cover})` }}></div>
             <div className="book-shelf-changer">
-              <select onChange={change} value={shelf || 'none'}>
-                <option value="move" disabled>Move to...</option>
-                {props.shelfs && props.shelfs.map((bookshelf, index) => (
-                  <option key={index} value={bookshelf.id}>{bookshelf.title}</option>
-                ))}
-                <option value="none">None</option>
-              </select>
+              {shelf && (
+                <ShelfSelector handleChange={this.handleChange} currentShelf={shelf || ''}/>
+              )}
             </div>
           </div>
           <div className="book-title">{title}</div>
@@ -45,8 +63,9 @@ const BookSnippet = (props) => {
               }
             }}>See details</Link>
         </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 BookSnippet.propTypes = {
