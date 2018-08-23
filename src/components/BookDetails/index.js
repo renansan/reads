4140bcdf839c0f5bf31bebec2758f35af0aa27ba
Renan San <renaansan@gmail.com>
2018-08-23 00:55:42 -0300

    Move each component to its own folder with its own test file

---
+++ src/components/BookDetails/index.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
+import Loading from './Loading'
+import ShelfSelector from './ShelfSelector'
+
+/**
+ * Book Details
+ * Show details from book
+ *
+ * @extends React
+ */
+class BookDetails extends React.Component {
+  state = {
+    book: '',
+    currentShelf: '',
+  }
+
+  //handle changes on ShelfSelector
+  handleChange = (event) => {
+    this.props.updateShelf(this.state.book, event);
+    this.setState({currentShelf: event.target.value});
+  };
+
+  componentDidMount() {
+    // const bookId = this.props.location.hash.replace('#', '');
+    // const bookId = this.props.location.state.bookId;
+
+    // Get the book ID from url passed by Route in props.location.
+    // Although it is possible to get the ID from parent component through props
+    // when access page by clicking in "See Details" anchor, It was the better
+    // approach to ensure that's the correct page when access it directly by URL
+    const bookId = this.props.location.search.replace('?id=', '');
+    // IF there's and ID in the URL get the book from API
+    if (bookId) {
+      BooksAPI.get(bookId).then(book => {
+        this.setState({ book });
+      });
+    // otherwise push back to root page
+    } else {
+      this.props.history.push('/');
+    }
+  }
+
+  render() {
+    // get constants from book state
+    const {title, subtitle, publisher, publishedDate, description, language, authors, imageLinks, pageCount, categories, canonicalVolumeLink} = this.state.book;
+    // set an object of details to loop through
+    const details = {publisher, publishedDate, language, pageCount, categories};
+    // check and set if a cover image exists
+    const cover = (imageLinks && imageLinks.thumbnail) ? imageLinks.thumbnail : '';
+    // set shelf from currentShelf (only if ShelfSelector changes) or from data taken from API
+    let shelf = this.state.currentShelf || this.state.book.shelf;
+    return (
+      <div className="book-page">
+        {!this.state.book ? (
+          <Loading />
+        ) : (
+          <article className="book-page-main">
+            {shelf && (
+              <ShelfSelector handleChange={this.handleChange} currentShelf={shelf || ''}/>
+            )}
+            <header className="book-page-header">
+              <div className="book-page-cover">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${cover})` }}></div>
+              </div>
+              <div className="book-page-abstract">
+                <h1 className="book-page-title">{title}</h1>
+                <h2 className="book-page-subtitle">{subtitle}</h2>
+                <div className="book-authors">{Array.isArray(authors) ? authors.join(', ') : authors}</div>
+                <dl className="book-details-list">
+                  {Object.keys(details).map((key, index) => (
+                    <div className="book-details-item" key={index}>
+                      <dt className="book-details-term">{key}:&nbsp;</dt>
+                      <dd className="book-details-description">{Array.isArray(details[key]) ? (details[key].join(', ')) : (details[key])}</dd>
+                    </div>
+                  ))}
+                </dl>
+                <a className="button" href={canonicalVolumeLink} title="Click to go to the store" target="_blank">Buy</a>
+              </div>
+            </header>
+            <div className="book-description">
+              <h3>Description</h3>
+              {description}
+            </div>
+          </article>
+        )}
+      </div>
+    )
+  }
+}
+
+BookDetails.propTypes = {
+  location: PropTypes.object.isRequired,
+  updateShelf: PropTypes.func.isRequired,
+}
+
+export default BookDetails
---
+++ src/components/Bookshelf/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import BookSnippet from './BookSnippet'
+import Loading from './Loading'
+import PropTypes from 'prop-types'
+import sortBy from 'sort-by'
+
+/**
+ * A shelf of books
+ * List all books marked with a determined shelf
+ *
+ * @param {Object} props
+ */
+const Bookshelf = (props) => {
+  // Filter books received from props (all books) to show only
+  // those listed in the current shelf
+  const books = (props.books) ? props.books.filter(book => book.shelf === props.id) : '';
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title} - <span className="bookshelf-title-count">{books.length} books</span></h2>
+      <div className="bookshelf-books">
+        {!Array.isArray(books) ? (
+            <Loading />
+        ) : !books.length ? (
+          <span>No books</span>
+        ) : (
+          <ol className="books-grid">
+            {books && books.sort(sortBy('title')).map((book, index) => (
+              <BookSnippet
+                key={index}
+                details={book}
+                shelfs={props.shelfs}
+                updateShelf={props.updateShelf}
+                />
+            ))}
+          </ol>
+        )}
+      </div>
+    </div>
+  )
+}
+
+Bookshelf.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  // books not required since PropTypes not allow check for null
+  // https://github.com/facebook/react/issues/3163#issuecomment-292669149
+  books: PropTypes.array,
+  shelfs: PropTypes.array.isRequired,
+  updateShelf: PropTypes.func.isRequired,
+}
+
+export default Bookshelf
