aecae5781d8826326f394761572d2f516ccc0a21
Renan San <renaansan@gmail.com>
2018-08-28 21:17:55 -0300

    Add tests for BooksList component

--- src/components/BookSnippet/test.js
+++ src/components/BookSnippet/test.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import App from '../App'
 
-it('do something', () => {})
+it('do something', () => {let test})
--- src/components/BooksList/test.js
+++ src/components/BooksList/test.js
@@ -1,5 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from '../App'
+import { shallow } from 'enzyme';
+import BooksList from './index'
+import Bookshelf from '../Bookshelf'
 
-it('do something', () => {})
+describe('tests suite', () => {
+  let props;
+  let wrapper;
+
+  describe('Base test', () => {
+    beforeEach(() => {
+      props = {
+        shelfs: [{"id": "test", "title": "Test Shelf"}],
+        books: [{"id": "test", "title": "Test Book", "shelf": "test"}],
+        updateShelf: jest.fn(() => 'updateShelf'),
+      };
+      wrapper = shallow(<BooksList {...props} />);
+    });
+
+    it('renders without crashing', () => {
+      expect(wrapper).toBeDefined();
+    });
+
+    it('renders Bookshelf with right props', () => {
+      expect(wrapper.find(Bookshelf)).toHaveLength(1);
+      expect(wrapper.find(Bookshelf)
+        .prop('title')).toEqual(props.shelfs[0].title);
+      expect(wrapper.find(Bookshelf)
+        .prop('books')).toContainEqual(props.books[0]);
+    });
+  });
+})
