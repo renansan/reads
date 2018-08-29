de1af4f6508ad88bb3dbee810702031f79f2b9ba
Renan San <renaansan@gmail.com>
2018-08-29 00:40:34 -0300

    fix bug when trying to change shelf
    When update shelf function was called, the state book was empty.

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -18,29 +18,32 @@ class BookDetails extends Component {
 
   //handle changes on ShelfSelector
   handleChange = (shelf) => {
-    this.props.updateShelf(this.props.details, shelf);
+    this.props.updateShelf(this.state.book, shelf);
     this.setState({currentShelf: shelf.id});
   };
 
-  componentDidMount() {
-    // const bookId = this.props.location.hash.replace('#', '');
-    // const bookId = this.props.location.state.bookId;
-
-    // Get the book ID from url passed by Route in props.location.
-    // Although it is possible to get the ID from parent component through props
-    // when access page by clicking in "See Details" anchor, It was the better
-    // approach to ensure that's the correct page when access it directly by URL
+  // Get the book ID from url passed by Route in props.location.
+  // Although it is possible to get the ID from parent component through props
+  // when access page by clicking in "See Details" anchor, It was the better
+  // approach to ensure that's the correct page when access it directly by URL
+  getBook = (cb) => {
     const bookId = this.props.location.search.replace('?id=', '');
     // IF there's and ID in the URL get the book from API
     if (bookId) {
       BooksAPI.get(bookId).then(book => {
-        this.setState({ book });
+        this.setState({ book }, () => cb(book) );
       });
     // otherwise push back to root page
-    } else {
-      this.props.history.push('/');
     }
   }
+
+  componentDidMount() {
+    // const bookId = this.props.location.hash.replace('#', '');
+    // const bookId = this.props.location.state.bookId;
+    this.getBook((book) => {
+      if (!book) this.props.history.push('/');
+    });
+}
 
   render() {
     // get constants from book state
--- src/components/Bookshelf/test.js
+++ src/components/Bookshelf/test.js
@@ -1,5 +1,34 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from '../App'
+import { shallow } from 'enzyme';
+import Bookshelf from './index'
+import BookSnippet from '../BookSnippet'
 
-it('do something', () => {let test})
+describe('tests suite', () => {
+  let props;
+  let wrapper;
+
+  describe('Base test', () => {
+    beforeEach(() => {
+      props = {
+        id: 'test',
+        title: 'Shelf Title',
+        books: [
+          {"id": "test", "title": "Test Book", "shelf": "test"},
+          {"id": "test2", "title": "Test Book 2", "shelf": "test2"},
+        ],
+        updateShelf: jest.fn(() => 'updateShelf'),
+      };
+      wrapper = shallow(<Bookshelf {...props} />);
+    });
+
+    it('renders without crashing', () => {
+      expect(wrapper).toBeDefined();
+    });
+
+    it('renders BookSnippet with right props', () => {
+      expect(wrapper.find(BookSnippet)).toHaveLength(1);
+      expect(wrapper.find(BookSnippet)
+        .prop('details')).toEqual(props.books[0]);
+    });
+  });
+})
